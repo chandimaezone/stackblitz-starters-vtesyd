e74057acb766983ec92dbd05b5092ec13691bf60
Chandima Ekanayake <chandima.ekanayake@ukg.com>
2023-08-09 15:03:29 +0530

    app com added

---
+++ src/app/app-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app-component/app-component';
+
+@NgModule({
+    imports: [],
+    exports: [AppComponent],
+    declarations: [AppComponent]
+})
+export class AppModule { }
